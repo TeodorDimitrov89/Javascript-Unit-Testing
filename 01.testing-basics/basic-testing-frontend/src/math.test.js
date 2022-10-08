f510e9352650e5851a6f17b38d49a4448a4e312b
TeodorDimitrov <teodordimitrov908@gmail.com>
2022-10-08 13:26:20 +0300

    Adding more test cases

--- 01.testing-basics/basic-testing-frontend/src/math.test.js
+++ 01.testing-basics/basic-testing-frontend/src/math.test.js
@@ -16,3 +16,52 @@ it("should summarize all number in an array", () => {
 
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  // Arrange
+  const inputs = ["invalid", 1];
+
+  //Act
+  const result = add(inputs);
+
+  //Asserts
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (currValue, nextValue) => +currValue + +nextValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield 0 if an empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into the function", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow();
+});
+
+it("should throw an error if provided with multiple arguments instead of an array", () => {
+  const num1 = 1;
+  const num2 = 2;
+  const resultFn = () => {
+    add(num1, num2);
+  };
+
+  expect(resultFn).toThrow();
+});
